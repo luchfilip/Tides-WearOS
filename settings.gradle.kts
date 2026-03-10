c13b91a55dc6e6ff8c4b16122b2df73009173326
Filip Luchianenco <filipluch@gmail.com>
2026-03-09 22:43:37 -0500

    build: initialize Gradle project with version catalog

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "tidal-wearos"
+include(":app")
+include(":core")
+include(":feature-auth")
+include(":feature-player")
+include(":feature-library")
+include(":feature-offline")
+include(":feature-settings")
