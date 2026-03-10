188ae9825077399a45f42b1b3c42cf82c112ebbc
Filip Luchianenco <filipluch@gmail.com>
2026-03-09 22:44:12 -0500

    feat(library): add DI module

---
+++ feature-library/src/main/kotlin/dev/tidesapp/wearos/library/di/LibraryModule.kt
@@ -0,0 +1,43 @@
+package dev.tidesapp.wearos.library.di
+
+import dev.tidesapp.wearos.library.data.api.TidesLibraryApi
+import dev.tidesapp.wearos.library.data.repository.AlbumRepositoryImpl
+import dev.tidesapp.wearos.library.data.repository.HomeRepositoryImpl
+import dev.tidesapp.wearos.library.data.repository.PlaylistRepositoryImpl
+import dev.tidesapp.wearos.library.data.repository.SearchRepositoryImpl
+import dev.tidesapp.wearos.library.domain.repository.AlbumRepository
+import dev.tidesapp.wearos.library.domain.repository.HomeRepository
+import dev.tidesapp.wearos.library.domain.repository.PlaylistRepository
+import dev.tidesapp.wearos.library.domain.repository.SearchRepository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LibraryRepositoryModule {
+
+    @Binds
+    abstract fun bindAlbumRepository(impl: AlbumRepositoryImpl): AlbumRepository
+
+    @Binds
+    abstract fun bindHomeRepository(impl: HomeRepositoryImpl): HomeRepository
+
+    @Binds
+    abstract fun bindPlaylistRepository(impl: PlaylistRepositoryImpl): PlaylistRepository
+
+    @Binds
+    abstract fun bindSearchRepository(impl: SearchRepositoryImpl): SearchRepository
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideTidesLibraryApi(retrofit: Retrofit): TidesLibraryApi {
+            return retrofit.create(TidesLibraryApi::class.java)
+        }
+    }
+}
