fc2066b45002aad0e250bb4734cf56c3a5c17c66
Filip Luchianenco <filipluch@gmail.com>
2026-03-09 22:43:46 -0500

    feat(core): add color palette

---
+++ core/src/main/kotlin/dev/tidesapp/wearos/core/ui/theme/Color.kt
@@ -0,0 +1,49 @@
+package dev.tidesapp.wearos.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Tides Design System Colors
+val TidesBlack = Color(0xFF000000)
+val TidesWhite = Color(0xFFFFFFFF)
+
+// Accent (Cyan)
+val TidesCyan = Color(0xFF21FEEC)
+val TidesCyanFill = Color(0xFF33FFEE)
+val TidesCyanBright = Color(0xFF66FFF3)
+val TidesCyanBrighter = Color(0xFF99FFF7)
+val TidesCyanDark = Color(0xFF3D8F88)
+val TidesCyanDarker = Color(0xFF244240)
+val TidesCyanDarkest = Color(0xFF141F1E)
+
+// Warning
+val TidesWarning = Color(0xFFFF3269)
+val TidesWarningBright = Color(0xFFF35E85)
+val TidesWarningDark = Color(0xFF99334E)
+
+// Error
+val TidesError = Color(0xFFE46962)
+
+// Success
+val TidesSuccess = Color(0xFF1EF299)
+
+// Gray
+val TidesGray = Color(0xFF72727F)
+val TidesGrayDarken15 = Color(0xFF505059)
+val TidesGrayDarken25 = Color(0xFF393940)
+val TidesGrayDarken35 = Color(0xFF222226)
+val TidesGrayDarken40 = Color(0xFF17171A)
+val TidesGrayDarken45 = Color(0xFF0C0C0D)
+
+// Glass (white with opacity)
+val TidesGlass10 = Color(0x1AEBEBFF)
+val TidesGlass20 = Color(0x33EBEBFF)
+val TidesGlass40 = Color(0x66EBEBFF)
+val TidesGlass60 = Color(0x99EBEBFF)
+val TidesGlass80 = Color(0xCCEBEBFF)
+
+// White opacity
+val TidesWhite10 = Color(0x1AFFFFFF)
+val TidesWhite20 = Color(0x33FFFFFF)
+val TidesWhite40 = Color(0x66FFFFFF)
+val TidesWhite60 = Color(0x99FFFFFF)
+val TidesWhite80 = Color(0xCCFFFFFF)
